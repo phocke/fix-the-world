d70a682c236dd39dc41ff79c36cb4f58f7b4512e
Przemek Hocke <phocke@gmail.com>
2010-08-18 09:59:56 +0200

    little fuckup removed

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,10 @@
 require File.expand_path('../boot', __FILE__)
-require ‘uri’
+require 'uri'
 
-if ENV[“MONGOHQ_URL”]
-  mongo_uri = URI.parse(ENV[“MONGOHQ_URL”])
-  ENV[‘MONGOID_HOST’] = mongo_uri.host
-  ENV[‘MONGOID_PORT’] = mongo_uri.port.to_s
-  ENV[“MONGOID_USERNAME”] = mongo_uri.user
-  ENV[‘MONGOID_PASSWORD’] = mongo_uri.password
-  ENV[‘MONGOID_DATABASE’] = mongo_uri.path.gsub(“/”, “”)
-end
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
-#require 'mongoid/railtie'
+require 'mongoid/railtie'
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
